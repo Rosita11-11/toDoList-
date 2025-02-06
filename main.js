5a7d41d0d30aced59d2d4e2fb2028164880422d6
rosita11-11 <itaaar44@gmail.com>
2025-02-06 16:39:07 +0700

    update

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-console.log('Hello World!');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYd6vdeRvwnywCPqeWh5PcdcNJj1kLfoo",
+    authDomain: "insancemerlang-2953a.firebaseapp.com",
+    projectId: "insancemerlang-2953a",
+    storageBucket: "insancemerlang-2953a.appspot.com",
+    messagingSenderId: "134418826980",
+    appId: "1:134418826980:web:ce438f43a3da1456b44ef1",
+    measurementId: "G-K6CVYVCMZP"
+}
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarStokOpname() {
+  const refDokumen = collection(db, "stokopname");
+  const kueri = query(refDokumen, orderBy("namabarang"));
+  const cuplikanKueri = await getDocs(kueri);
+  
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id, 
+      namabarang: dok.data().namabarang,
+      jumlahbarang: dok.data().jumlahbarang,
+      sisabarang: dok.data().sisabarang
+    });
+  });
