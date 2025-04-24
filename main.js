2f823c7dd3bb7ad7ed1f99950577910677ace1da
rosita11-11 <itaaar44@gmail.com>
2025-04-24 20:56:53 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYd6vdeRvwnywCPqeWh5PcdcNJj1kLfoo",
+    authDomain: "insancemerlang-2953a.firebaseapp.com",
+    projectId: "insancemerlang-2953a",
+    storageBucket: "insancemerlang-2953a.appspot.com",
+    messagingSenderId: "134418826980",
+    appId: "1:134418826980:web:ce438f43a3da1456b44ef1",
+    measurementId: "G-K6CVYVCMZP"
+}
 let tasks = [];
 
 function addTask() {
